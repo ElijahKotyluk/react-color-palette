f5bcf39b148f5df133199ad0d0955f010279bb2f
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-02-05 17:53:01 -0800

    feat(color-palette): add color-palette component
    Add mock data file.

---
+++ src/sampleColors.ts
@@ -0,0 +1,28 @@
+export default [
+    {
+        paletteName: "Dark UI",
+        id: "dark-ui",
+        colors: [
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+            { name: "red", color: "#F44336" },
+        ]
+    }
+]
